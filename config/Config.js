dfd3a771f8ca6ff7dd4f5acd2850304b0fedb26c
Bibek Rimal <62979302+bbk-rml@users.noreply.github.com>
2023-11-14 15:52:51 +1100

    firebase app config

---
+++ config/Config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCakBPtrPD33u6AP8laOwaPCeYE4xY2y_g",
+    authDomain: "todo-52760.firebaseapp.com",
+    projectId: "todo-52760",
+    storageBucket: "todo-52760.appspot.com",
+    messagingSenderId: "121307194844",
+    appId: "1:121307194844:web:674d12e87fe65b27661b19"
+  };
